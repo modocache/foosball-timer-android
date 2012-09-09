a33b77dfadb3efba4a71b6e9eaf0d7804acf0e10
modocache <modocache@gmail.com>
2012-09-09 12:07:27 -0400

    Use fragments.
    - Update min-sdk value to 11.
    - Remove suppress deprecation warning directives.
    - Use fragments as per new API.

--- src/com/modocache/android/foosballtimer/FoosballTimerActivity.java
+++ src/com/modocache/android/foosballtimer/FoosballTimerActivity.java
@@ -2,9 +2,8 @@ package com.modocache.android.foosballtimer;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.app.Dialog;
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Toast;
@@ -12,9 +11,6 @@ import android.widget.Toast;
 public class FoosballTimerActivity extends Activity {
 
     static private final int gameMaximumTime = 240;
-    static private final int gameProgressDialogIdentifier = 0;
-    ProgressDialog gameProgressDialog;
-    private Thread gameProgressUpdateThread;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,67 +24,19 @@ public class FoosballTimerActivity extends Activity {
         return true;
     }
 
-    @Override
-    @Deprecated
-    protected Dialog onCreateDialog(int id) {
-        switch (id) {
-        case gameProgressDialogIdentifier:
-            gameProgressDialog = new ProgressDialog(this);
-            gameProgressDialog.setTitle(getString(R.string.game_progress_dialog_title));
-            gameProgressDialog.setMessage(getString(R.string.game_progress_dialog_message));
-            gameProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-            gameProgressDialog.setMax(gameMaximumTime);
-            gameProgressDialog.setButton(ProgressDialog.BUTTON_NEGATIVE,
-                                         getString(R.string.game_progress_dialog_cancel_button_text),
-                                         new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    stopGame();
-                }
-            });
-            return gameProgressDialog;
-        default:
-            break;
-        }
-        return null;
-    }
-
-    @SuppressWarnings("deprecation")
     public void onStartGameButtonClick(View button) {
         Toast.makeText(getBaseContext(),
                        getString(R.string.start_game_toast_text),
                        Toast.LENGTH_SHORT).show();
-        showDialog(gameProgressDialogIdentifier);
-        gameProgressDialog.setProgress(0);
-        gameProgressUpdateThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    for (int i = 0; i < gameMaximumTime; i++) {
-                        Thread.sleep(1000);
-                        gameProgressDialog.incrementProgressBy(1);
-                    }
-                    stopGame();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        gameProgressUpdateThread.start();
-    }
 
-    private void stopGame() {
-        gameProgressUpdateThread.interrupt();
-        gameProgressUpdateThread = null;
-        gameProgressDialog.dismiss();
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        Fragment prev = getFragmentManager().findFragmentByTag("dialog");
+        if (prev != null) {
+            fragmentTransaction.remove(prev);
+        }
+        fragmentTransaction.addToBackStack(null);
 
-        this.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                Toast.makeText(getBaseContext(),
-                               getString(R.string.end_game_toast_text),
-                               Toast.LENGTH_SHORT).show();
-            }
-        });
+        GameProgressFragment gameProgressFragment = GameProgressFragment.newInstance(gameMaximumTime);
+        gameProgressFragment.show(fragmentTransaction, "dialog");
     }
 }
---
+++ src/com/modocache/android/foosballtimer/GameProgressFragment.java
@@ -0,0 +1,80 @@
+package com.modocache.android.foosballtimer;
+
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Toast;
+import android.app.DialogFragment;
+
+public class GameProgressFragment extends DialogFragment {
+
+    static private final int defaultGameMaximumTime = 240;
+    static ProgressDialog gameProgressDialog;
+    private Thread gameProgressUpdateThread;
+
+    static GameProgressFragment newInstance(int gameMaximumTime) {
+        GameProgressFragment fragment = new GameProgressFragment();
+        Bundle args = new Bundle();
+        args.putInt("gameMaximumTime", gameMaximumTime);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        gameProgressDialog = new ProgressDialog(getActivity());
+        gameProgressDialog.setTitle(getString(R.string.game_progress_dialog_title));
+        gameProgressDialog.setMessage(getString(R.string.game_progress_dialog_message));
+        gameProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        gameProgressDialog.setMax(getArguments().getInt("gameMaximumTime", defaultGameMaximumTime));
+        gameProgressDialog.setButton(ProgressDialog.BUTTON_NEGATIVE,
+                                     getString(R.string.game_progress_dialog_cancel_button_text),
+                                     new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dismiss();
+            }
+        });
+        return gameProgressDialog;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        gameProgressDialog.setProgress(0);
+        gameProgressUpdateThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int gameMaximumTime = getArguments().getInt("gameMaximumTime", defaultGameMaximumTime);
+                try {
+                    for (int i = gameProgressDialog.getProgress(); i < gameMaximumTime; i++) {
+                        Thread.sleep(1000);
+                        gameProgressDialog.incrementProgressBy(1);
+                    }
+                    dismiss();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        gameProgressUpdateThread.start();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        gameProgressUpdateThread.interrupt();
+        gameProgressUpdateThread = null;
+        dismiss();
+
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getActivity().getBaseContext(),
+                               getString(R.string.end_game_toast_text),
+                               Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
